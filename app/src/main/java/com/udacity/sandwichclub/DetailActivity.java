bf7ee9d88884323c20f0df1987a284a2977b14c3
Daniel Burgner <dburgnerjr@yahoo.com>
2020-03-10 21:33:23 -0500

    D60/D61-2020 Migrated to AndroidX
    D60/D61 of #100DaysOfCode #2020 challenge.  Migrated #SandwichClub app to #AndroidX #OperationCleanCode #AndroidDev #BuiltByUdacity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,9 +1,9 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,8 +12,6 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
-
-import java.util.List;
 
 import org.json.JSONException;
 
